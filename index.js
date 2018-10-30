7ff365f1079785f98e786f774a860125c4e181d1
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-30 14:45:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
 let neighborhoodIds = 0
-let customerIds = 0 
-let mealIds = 0 
-let deliveryIds = 0 
+let customerIds = 0
+let mealIds = 0
+let deliveryIds = 0
+
+class Neighborhood {
+  
+}

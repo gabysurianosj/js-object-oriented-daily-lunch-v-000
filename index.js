4e57f1b2c92f923f3d8ada065d6258f331393571
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-30 14:45:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let neighborhoodIds = 0
+let customerIds = 0 
+let mealIds = 0 
+let deliveryIds = 0 
